5f6f01f03ffaf2f76d947bb42e729f25b639c1f7
Marge <marge.consunji@gmail.com>
2021-03-24 13:06:48 +0800

    Basic note schema

---
+++ sanity/schemas/note.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'note',
+    title: 'Note',
+    type: 'document',
+    fields: [
+        {
+            title: 'Title',
+            name: 'title',
+            type: 'string',
+        },
+        {
+            title: 'Slug',
+            name: 'slug',
+            type: 'slug'
+        }
+    ]
+}
